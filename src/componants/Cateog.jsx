e89802b867c42ab9e19a963376405e6dc3604f02
mohamed <mhalwasoffice@gmail.com>
2024-10-24 20:24:21 +0100

    updating

--- src/componants/Cateog.jsx
+++ src/componants/Cateog.jsx
@@ -1,9 +1,10 @@
-import Products from "./products.jsx";
+import Product1 from "./product1.jsx";
+import Product2 from "./product2.jsx";
 import prod1 from '../assets/products/prod1.svg';
 
 export default function Catego(){
     return (
-        <div className="p-2">
+        <div className="p-2 w-full">
             <h2 className="font-bold text-2xl mb-4">Catégories</h2>
             <div className="flex gap-2 mb-4">
                 <span className="bg-white cursor-pointer px-4 py-2 rounded-2xl shadow-sm">Tous</span>
@@ -15,9 +16,9 @@ export default function Catego(){
                 <span className="bg-white cursor-pointer px-4 py-2 rounded-2xl shadow-sm">Accessoires</span>
             </div>
             <h2 className="font-bold text-2xl mb-4">Liste des Pantalons</h2>
-            <div className="flex flex-wrap gap-6">
-                <Products img={prod1} title='Pantalon en Jean Léger' type='Pantalons' prix={359}/>
-                <Products img={prod1} title='Pantalon en Jean Léger' type='Pantalons' prix={359}/>
+            <div className="flex flex-wrap gap-6 bg-red-400 w-full">
+                <Product1 img={prod1} title='Pantalon en Jean Léger' type='Pantalons' prix={359}/>
+                <Product2 img={prod1} title='Pantalon en Jean Léger' type='Pantalons' prix={359}/>
             </div>
 
         </div>
--- src/componants/product1.jsx
+++ src/componants/product1.jsx
@@ -1,7 +1,7 @@
-export default function Products({ type, img, prix, title }) {
+export default function Product1({ type, img, prix, title }) {
   return (
     <div
-      className="bg-white flex flex-col gap-5 p-5 rounded-sm "
+      className="bg-white w-[30%] flex flex-col gap-5 p-5 rounded-xl "
       style={{
         boxShadow: "0px 10px 30px rgba(0,0,0,.1",
       }}
@@ -14,35 +14,43 @@ export default function Products({ type, img, prix, title }) {
           <p className="text-2xl font-semibold">{prix} Dh</p>
         </span>
       </div>
-      <div>
+      <div className="w-[80%]">
         <h2 className="text-lg">Couleur</h2>
-        <span className="flex flex-wrap gap-1">
-          <span className="flex items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
+        <span className="flex  flex-wrap gap-1">
+          <span className="flex cursor-pointer items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
             <div className="w-4 h-4 bg-black rounded-full">
             
             </div>
             Noir
           </span>
-          <span className="flex items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
+          <span className="flex cursor-pointer items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
             <div className="w-4 h-4 bg-white rounded-full">
             
             </div>
             Blanc
           </span>
-          <span className="flex items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
+          <span className="flex cursor-pointer items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
             <div className="w-4 h-4 bg-[#0D0072] rounded-full">
             
             </div>
             Bleu foncé
           </span>
-          <span className="flex items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
+          <span className="flex cursor-pointer items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
             <div className="w-4 h-4 bg-[#965E47] rounded-full">
             
             </div>
             Marron
           </span>
+          <span className="flex cursor-pointer items-center gap-1 px-4 rounded-full py-1 bg-[#F2F2F2]">
+            <div className="w-4 h-4 bg-[#007F1B] rounded-full">
+            
+            </div>
+            Vert
+          </span>
         </span>
       </div>
+      <hr></hr>
+      <button className="bg-primary text-white py-3 rounded-lg">Ajouter au panier</button>
     </div>
   );
 }
